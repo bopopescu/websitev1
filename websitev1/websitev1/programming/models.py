0775e66465351cba5d5bbb41d6e330097043041b
Mustafa <hazim6163@gmail.com>
2018-06-13 17:56:42 -0800

    create language, Lists,  Articles models .

--- websitev1/websitev1/programming/models.py
+++ websitev1/websitev1/programming/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+#create the language model
+class Language(models.Model):
+    """Table for all languages"""
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    pubDate = models.DateTimeField(default=datetime.now, blank=True)
+    slug = models.CharField(max_length=255, blank=True)
+    position = models.IntegerField(unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Lists(models.Model):
+    """ lists Table"""
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    language_name = language.name
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    pubDate = models.DateTimeField(default=datetime.now, blank=True)
+    slug = models.CharField(max_length=255, blank=True)
+    position = models.IntegerField(unique=True, blank=True)
+
+    def __str__(self):
+        """name will returned in Admin panel"""
+        return self.name
+
+class Articles(models.Model):
+    """ Articles Table """
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    article_list = models.ForeignKey(Lists, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    body = models.TextField()
+    pubDate = models.DateTimeField(default=datetime.now, blank=True)
+    slug = models.CharField(max_length=255, blank=True)
+    position = models.IntegerField(unique=True, blank=True)
+    language_name = language.name
+    list_name = article_list.name
+
+    def __str__(self):
+        return self.title
--- websitev1/websitev1/programming/views.py
+++ websitev1/websitev1/programming/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .models import (Language, Lists, Articles)
 
 def index(request):
     return HttpResponse('Hello From the Other Side')
